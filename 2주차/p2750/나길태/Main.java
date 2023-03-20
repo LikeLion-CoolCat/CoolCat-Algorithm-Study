41707ed93c247482e7e71c3fe2a122e6c9a47fcc
GilTae-Na <106876965+GilTae-Na@users.noreply.github.com>
2023-03-20 17:58:06 +0900

    2750 버블 나길태
    .

---
+++ 2주차/p2750/나길태/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        } //입력받기
+
+        for (int i = 0; i < n - 1; i++) { //버블
+            for (int j; i < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
