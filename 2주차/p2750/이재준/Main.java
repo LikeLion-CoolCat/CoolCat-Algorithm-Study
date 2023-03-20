7c8733eb2ae36445def5259bab29755e64e4f33e
Jaejun Lee <110995932+jooooonj@users.noreply.github.com>
2023-03-20 17:45:50 +0900

    [BOJ] 2164 / Bronze2

---
+++ 2주차/p2750/이재준/Main.java
@@ -0,0 +1,30 @@
+package 백준.Baekjoon2164;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for(int num : arr)
+            System.out.println(num);
+    }
+}
