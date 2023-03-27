cf7b265a69ebd899816973c4867ab451339f5524
tkdtn4657 <75903442+tkdtn4657@users.noreply.github.com>
2023-03-27 17:54:46 +0900

    p1260문제

---
+++ 3주차/p1260/이상수/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {// [BOJ] 1260문제 입력도 제대로 못받음...
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int first = sc.nextInt();
+        
+        int[] a = new int[n];
+        int[] b = new int[m];
+        for(int i = 0; i < n; i++){
+            
+        }
+        
+
+    }
+}
