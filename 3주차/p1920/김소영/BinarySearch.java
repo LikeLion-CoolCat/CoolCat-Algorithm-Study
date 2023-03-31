1a1997bc01a6be8ca0cba79266aa870e27726fd8
whale22 <realwhale64@naver.com>
2023-03-31 16:31:10 +0900

    Update BinarySearch.java

--- 3주차/p1920/김소영/BinarySearch.java
+++ 3주차/p1920/김소영/BinarySearch.java
@@ -14,17 +14,20 @@ public class Main {
             tempArr[i]=sc.nextInt();
         }
 
+        Arrays.sort(arr); // 정렬
         System.out.print("검색 결과 : ");
-        binarySearch(arr, tempArr);
+        for(int i=0;i<sizeT;i++){
+            System.out.print(binarySearch(arr, tempArr[i], 0, size-1)+" ");
+        }
 
     }
 
-    public static void binarySearch(int[] arr, int[] temp){
-        Arrays.sort(arr); // 정렬
-        for(int i=0;i<temp.length;i++){
-            //구현 필요. . .
-        }
-
+    public static int binarySearch(int[] arr, int temp, int low, int high){
+        if(low>high) return 0;
+        int mid = (low+high)/2;
+        if(arr[mid]<temp) return binarySearch(arr, temp, mid+1,high);
+        else if (arr[mid]>temp) return binarySearch(arr, temp, low, temp-1);
+        else return 1;
     }
 
 }
