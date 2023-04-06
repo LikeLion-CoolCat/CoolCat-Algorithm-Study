7e57bc96eec97699dd8f0501ea9c58c530668f76
whale22 <realwhale64@naver.com>
2023-04-06 23:32:23 +0900

    Greedy를 입력한 최소값 구하기 구현 완료

--- 4주차/p1541/GreedyHard.java
+++ 4주차/p1541/GreedyHard.java
@@ -8,35 +8,40 @@ public class Main {
         String formula = sc.nextLine();
         String[] tempForm;
         int sum = 0;
-        if(!formula.contains("-")) {
-            // -가 하나도 없는 경우, 그냥 계산하면 됨
-            sum=sumcalc(formula);
-        } else if (!formula.contains("\\+")) {
-            // +가 하나도 없는 경우, 모든 수를 더해서 마이너스만 곱해주면 됨
-            sum=sumcalc(formula.replaceAll("-","+"));
-            sum*=-1;
-        } else {
-            //섞여있는 경우
-        }
-
+        sum=recalc(formula);
         System.out.println("결과 : "+sum);
     }
 
     public static int recalc(String formula){
         int sum=0;
-        String[] tempForm = formula.split("\\+");
-        for(String s : tempForm){
-            if(!s.equals("")) sum+=Integer.parseInt(s);
+        if(!formula.contains("-")){
+            //+로만 이어진 수식
+            return sumcalc(formula, true);
+        } else if (!formula.contains("+")) {
+            //-로만 이어진 수식
+            return sumcalc(formula.replaceAll("-","+"), false);
+        } else {
+            //복합식
+            String[] tempForm = formula.split("-",2);
+            if(tempForm[0].equals("")){
+                // -로 시작하는 식은 사실상 전부 음수 값을 리턴한다
+                return sumcalc(formula.replaceAll("-","+"), false);
+            }else {
+                // 양수로 시작하는 식은 사실상 양수항의 합에서 가장 첫번째 -로 시작하는 식을 뺸 값을 더한 것과 같다
+                return sumcalc(tempForm[0],true) + sumcalc(tempForm[1].replaceAll("-","+"), false);
+            }
         }
-        return sum;
+
     }
 
-    public static int sumcalc(String formula){
+    public static int sumcalc(String formula, boolean positive){
         int sum=0;
+        if(!formula.contains("+"))
+            return Integer.parseInt(formula);
         String[] tempForm = formula.split("\\+");
         for(String s : tempForm){
             if(!s.equals("")) sum+=Integer.parseInt(s);
         }
-        return sum;
+        return (positive==true)? sum : sum*-1; // 양수 표기가 true면 양수의  합을 반환, 그게 아니면 음수의 합을 반환
     }
 }
