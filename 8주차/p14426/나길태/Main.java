e74cdd5915f0a2752ba011dc668ff471a5804f1e
GilTae-Na <106876965+GilTae-Na@users.noreply.github.com>
2023-05-01 17:34:59 +0900

    P14426

---
+++ 8주차/p14426/나길태/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        int count=0;
+
+        String [] arr = new String [N];
+
+        for(int i=0;i<N;i++) {
+            arr[i] = br.readLine();
+        }
+
+        for(int i=0;i<M;i++) {
+            String str = br.readLine();
+            for(int j=0;j<N;j++)
+                if(arr[j].startsWith(str)) {
+                    count++;
+                    break;
+                }
+        }
+
+        System.out.println(count);
+    }
+}
